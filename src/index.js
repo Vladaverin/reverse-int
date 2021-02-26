82789fb4dbdc272a858fcb77369e5fbabf1106d4
VladAverin <Vladaverin@users.noreply.github.com>
2021-02-27 00:45:07 +0300

    fix: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var digit, result = 0
+
+    while (n) {
+        digit = n % 10
+        result = (result * 10) + digit
+        n = n / 10 | 0
+    }
+    return result
 }
